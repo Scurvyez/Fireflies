251e1cb3bbdd184607c68667270ff77cf174ada7
Maxim <MaximSw@web.de>
2021-09-04 15:03:31 +0200

    Animation Push
    - Adding animation to fireflies

---
+++ Source/Fireflies/Firefly.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace Fireflies
+{
+    public class Firefly : Pawn
+    {
+        private Comp_Firefly compFirefly;
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            compFirefly = this.TryGetComp<Comp_Firefly>();
+        }
+
+        public override Vector3 DrawPos => base.DrawPos + (compFirefly?.FireflyPos ?? Vector3.zero);
+    }
+}
